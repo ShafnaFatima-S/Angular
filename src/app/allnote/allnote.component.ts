a7a015006511461aa591de938bb206f6efde24b7
unknown <shafnafatima248@gmail.com>
2025-04-23 10:53:04 +0530

    new commit

--- src/app/allnote/allnote.component.ts
+++ src/app/allnote/allnote.component.ts
@@ -37,11 +37,50 @@ async fetchNotes(){
    let response= await axios.get(url,{headers: {
     Authorization: `Bearer ${token[0]}`
 }})
+console.log(response.data)
 this.notes=Object.values(response.data.data)
 console.log("notes======>",this.notes)
 }
     
+async trash(){
+  console.log("trash token===>",token)
+  const url=`http://localhost:8080/last30Days`
+  console.log("trash url=======>",url)
+  const response=await axios.get(url,{headers:{
+    Authorization: `Bearer ${token[0]}`
+  }} )
+   this.notes=Object.values(response.data.data)
+  console.log("trash response=======>",response.data.data)
+  
+}
+async pin(id:string){
+  console.log("pinned token----->",token)
+  const url=`http://localhost:8080/pin?id=${id}`
+  console.log("Pinned url======>",url)
+  const response=await axios.get(url,{headers:{
+    Authorization: `Bearer ${token[0]}`
+  }})
+  console.log("pinned response=======>",response.data)
+  // this.notes=response.data
+ this.fetchNotes()
 
+  
+}
+
+async click(id:string){
+//   console.log("token=====>",token)
+//   const url =`http://localhost:8080/getAll`
+//    console.log("click url============>",{url})
+//    let response= await axios.get(url,{headers: {
+//     Authorization: `Bearer ${token[0]}`
+// }})
+// console.log(response.data)
+// this.notes=Object.values(response.data.data)
+// const id=response.data.data[0].id
+console.log("id:",id)
+// console.log("click notes======>",this.notes)
+
+}
 // input:any=[{title:'work',description:'complete it',created:'07/04/2025'},
 //   {title:'work',description:'complete all szdxfcvh cfvbhjn dcfvbn cfvgbh fvgbh',created:'07/04/2025'},
 //   {title:'asdfgh',description:'complete_all',created:'07/04/2025'},
